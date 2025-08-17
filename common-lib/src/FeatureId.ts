44209b0eb73e8e4d61f9681717d9d61c2a169906
Braden MacDonald <mail@bradenm.com>
2025-08-17 11:16:00 -0700

    feat: add a common-lib and a FeatureId parser

---
+++ common-lib/src/FeatureId.ts
@@ -0,0 +1,181 @@
+/**
+ * Our overly-complicated FeatureId format.
+ *
+ * A FeatureId is a unique Identifier for anything that can appear on the map,
+ * and perhaps even abstract things like "schedules" that can't exactly be mapped
+ * but are nevertheless part of Transitopia.
+ *
+ * The basic format of a FeatureId is designed to exaclty match what users see in
+ * the URL, e.g.
+ *    /cycling/way/123890
+ * for a bike lane, which would be accessed at https://www.transitopia.org/cycling/way/123890
+ *
+ * The main thing about FeatureIds is that we always try to just re-use the "main"
+ * external identifier. So for bike lanes, we always use the OpenStreetMap "way" ID
+ * and so on.
+ *
+ * However, sometimes we need to namespace our IDs. For example, bike parking spaces
+ * come primarily from OpenStreetMap, but there are separate namespaces for them:
+ * "node" bike parking spaces and "way" bike parking areas. Because the ID numbers
+ * are only unique within each namespace, we have to distinguish them like this:
+ *    /cycling/parking/node:123890
+ *    /cycling/parking/way:123890   (a totally different bike parking area)
+ *
+ * And then, we may in the future have data coming in from other sources than the
+ * "main" source for a given type, so we support a "source" prefix like this:
+ *    /cycling/station/gbfs:vancouver:123325
+ *    /cycling/station/other_source:vancouver:acbdef
+ *
+ * So the full breakdown is:
+ *    /domain/type/source:namespace:identifier
+ *
+ * Where source and/or namespace are often optional, depending on the domain+type.
+ * If source is included, namespace is always required.
+ */
+import * as z from "zod";
+
+export const FeatureDomain = {
+  /** Cycling and micromobility */
+  Cycling: "cycling",
+  /** Public Transit */
+  Transit: "transit",
+} as const;
+const FeatureDomainEnum = z.enum(FeatureDomain);
+export type FeatureDomain = z.infer<typeof FeatureDomainEnum>;
+
+/** The meaning of these feature types depends on the domain */
+export const FeatureType = {
+  Way: "way",
+  Parking: "parking",
+  Route: "route",
+  Station: "station",
+  Bike: "bike",
+} as const;
+const FeatureTypeEnum = z.enum(FeatureType);
+export type FeatureType = z.infer<typeof FeatureTypeEnum>;
+
+export const FeatureSource = {
+  /** OpenStreetMap */
+  Osm: "osm",
+  /** General Transit Feed Specification */
+  Gtfs: "gtfs",
+  /** General Bikeshare Feed Specification  */
+  Gbfs: "gbfs",
+} as const;
+const FeatureSourceEnum = z.enum(FeatureSource);
+export type FeatureSource = z.infer<typeof FeatureSourceEnum>;
+
+/** The three types of features used on OpenStreetMap: Node, Way, and Relation */
+export const OsmFeatureNamespace = {
+  Node: "node",
+  Way: "way",
+  Relation: "relation",
+} as const;
+const OsmFeatureNamespaceEnum = z.enum(OsmFeatureNamespace);
+export type OsmFeatureNamespace = z.infer<typeof OsmFeatureNamespaceEnum>;
+
+/** A bike lane / bike track */
+const CyclingWayId = z.object({
+  domain: z.literal(FeatureDomain.Cycling),
+  type: z.literal(FeatureType.Way),
+  source: z.literal(FeatureSource.Osm).default(FeatureSource.Osm),
+  namespace: z
+    .literal(OsmFeatureNamespace.Way)
+    .default(OsmFeatureNamespace.Way),
+  identifier: z.string().regex(/^\d+$/),
+});
+export type CyclingWayId = z.infer<typeof CyclingWayId>;
+
+/** A bike parking spot */
+const BicycleParkingId = z.object({
+  domain: z.literal(FeatureDomain.Cycling),
+  type: z.literal(FeatureType.Parking),
+  source: z.literal(FeatureSource.Osm).default(FeatureSource.Osm),
+  namespace: z.union([
+    z.literal(OsmFeatureNamespace.Node),
+    z.literal(OsmFeatureNamespace.Way),
+  ]),
+  identifier: z.string().regex(/^\d+$/),
+});
+export type BicycleParkingId = z.infer<typeof BicycleParkingId>;
+
+/** A bike route, e.g. "Central Valley Greenway" - a path made of many CyclingWay segments */
+const CyclingRouteId = z.object({
+  domain: z.literal(FeatureDomain.Cycling),
+  type: z.literal(FeatureType.Route),
+  source: z.literal(FeatureSource.Osm).default(FeatureSource.Osm),
+  namespace: z
+    .literal(OsmFeatureNamespace.Relation)
+    .default(OsmFeatureNamespace.Relation),
+  identifier: z.string().regex(/^\d+$/),
+});
+export type CyclingRouteId = z.infer<typeof CyclingRouteId>;
+
+const CyclingFeatureId = z.discriminatedUnion("type", [
+  CyclingWayId,
+  CyclingRouteId,
+  BicycleParkingId,
+]);
+
+const TransitRouteId = z.object({
+  domain: z.literal(FeatureDomain.Transit),
+  type: z.literal(FeatureType.Route),
+  source: z.literal(FeatureSource.Gtfs).default(FeatureSource.Gtfs),
+  namespace: z.string(),
+  identifier: z.string().regex(/^\d+$/),
+});
+const TransitStationId = z.object({
+  domain: z.literal(FeatureDomain.Transit),
+  type: z.literal(FeatureType.Station),
+  source: z.literal(FeatureSource.Gtfs).default(FeatureSource.Gtfs),
+  namespace: z.string(),
+  identifier: z.string().regex(/^\d+$/),
+});
+
+const TransitFeatureId = z.discriminatedUnion("type", [
+  TransitRouteId,
+  TransitStationId,
+]);
+
+export const FeatureId = z.discriminatedUnion("domain", [
+  CyclingFeatureId,
+  TransitFeatureId,
+]);
+export type FeatureId = z.infer<typeof FeatureId>;
+
+// Make parseIdString() return a specific FeatureId subclass if it can
+export function parseIdString<
+  D extends FeatureDomain = FeatureDomain,
+  T extends FeatureType = FeatureType,
+>(idStr: `${D}/${T}/${string}`): FeatureId & { domain: D; type: T };
+
+export function parseIdString(idStr: string): FeatureId;
+
+export function parseIdString(idStr: string) {
+  const [domain, type, sourceNamespaceIdentifier, ...rest] = idStr.split("/");
+  if (rest.length !== 0) {
+    throw new Error(`Invalid ID "${idStr}": unexpected "/"`);
+  }
+  const [first, second, ...idParts] = sourceNamespaceIdentifier.split(":");
+  let source: string | undefined,
+    namespace: string | undefined,
+    identifier: string | undefined;
+  if (idParts.length > 0) {
+    source = first;
+    namespace = second;
+    identifier = idParts.join(":");
+  } else if (second !== undefined) {
+    namespace = first;
+    identifier = second;
+  } else {
+    identifier = first;
+  }
+  return FeatureId.parse({
+    domain,
+    type,
+    source,
+    namespace,
+    identifier,
+    // deno-lint-ignore no-explicit-any
+  }) as any;
+}
---
+++ common-lib/src/utils/type-asserts.ts
@@ -0,0 +1,105 @@
+//========== About this module ==========
+
+// This is https://github.com/rauschma/asserttt/blob/main/src/asserttt.ts
+// By Axel Rauschmayer
+
+// MIT License
+
+// Copyright (c) 2025 Axel Rauschmayer
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+//========== Asserting ==========
+
+/**
+ * - Is type `_T` assignable to `true`
+ * - Based on code by Blaine Bublitz.
+ */
+export type Assert<_T extends true> = void;
+
+/**
+ * Is the type of `_value` assignable to `T`?
+ */
+export function assertType<T>(_value: T): void {}
+
+//========== Predicates: equality ==========
+
+/**
+ * - Is type `X` equal to type `Y` (with `any` only being equal to itself)?
+ * - Name motivated by Node’s assert.equal().
+ * - Like `MutuallyAssignable` but `any` is only equal to itself.
+ */
+export type Equal<X, Y> = [IsAny<X>, IsAny<Y>] extends [true, true] ? true
+  : [IsAny<X>, IsAny<Y>] extends [false, false] ? MutuallyAssignable<X, Y>
+  : false;
+
+/**
+ * - The brackets on the left-hand side of `extends` prevent
+ *   distributivity.
+ */
+export type MutuallyAssignable<X, Y> = [X, Y] extends [Y, X] ? true : false;
+
+/**
+ * - Name motivated by Node’s assert.deepEqual().
+ * - Source: https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
+ */
+export type PedanticEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false;
+
+//========== Predicates: comparing types ==========
+
+/**
+ * - Does type `Sub` extend type `Super`?
+ * - Square brackets around `Sub` prevent distributivity (`Super` is also
+ *   in brackets so that the test works).
+ */
+export type Extends<Sub, Super> = [Sub] extends [Super] ? true : false;
+
+/**
+ * - Is type `Target` assignable from type `Source`?
+ * - Square brackets around `Source` prevent distributivity (`Target` is
+ *   also in brackets so that the test works).
+ */
+export type Assignable<Target, Source> = [Source] extends [Target] ? true
+  : false;
+
+/**
+ * - Is type `Subset` a subset of type `Superset`?
+ * - Square brackets around `Subset` prevent distributivity (`Superset` is
+ *   also in brackets so that the test works).
+ */
+export type Includes<Superset, Subset> = [Subset] extends [Superset] ? true
+  : false;
+
+//========== Predicates: boolean operations ==========
+
+/**
+ * Boolean NOT for type `B`.
+ */
+export type Not<B extends boolean> =
+  // `Equal` because want to avoid Not<any> being `false` or `true`
+  Equal<B, true> extends true ? false
+    : (Equal<B, false> extends true ? true : never);
+
+//========== Predicates: other ==========
+
+/**
+ * - Source: https://stackoverflow.com/questions/49927523/disallow-call-with-any/49928360#49928360
+ */
+export type IsAny<T> = 0 extends (1 & T) ? true : false;
